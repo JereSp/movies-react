5a433d26640f11539e7c618272c5515790e95257
JereSp <jeremiasnumx@gmail.com>
2021-09-23 21:00:05 -0300

    tercer comit

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,17 +1,30 @@
 import { MovieCard } from './MovieCard';
+import { useQuery } from '../hooks/useQuery';
 import { useEffect, useState } from 'react';
 import styles from './MoviesGrid.module.css'
 import { get } from '../utils/httpClient';
-
+import { Spinner } from './Spinner';
 
 export function MoviesGrid() {
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
+    const query = useQuery();
+    const search = query.get("search")
+    
     useEffect(() => {
-        get("/discover/movie").then(data => {
+        setIsLoading(true);
+        const searchUrl = search ? "/search/movie?query=" + search : "/discover/movie"
+        get(searchUrl).then(data => {
             setMovies(data.results);
+            setIsLoading(false);
         });
-    }, []);
+    }, [search]);
+
+    if (isLoading) {
+        return <Spinner/>
+    }
+
     return (
         <ul className={styles.moviesGrid}>       
             {movies.map((movie) => (
